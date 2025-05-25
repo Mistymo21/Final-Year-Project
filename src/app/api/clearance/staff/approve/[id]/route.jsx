682f519a49ff112f426286bf9d6c341722ff58bb
Mistymo21 <yusufmm90999@gmail.com>
2025-05-25 14:48:57 +0100

    Temporary  edit 3

--- src/app/api/clearance/staff/approve/[id]/route.jsx
+++ src/app/api/clearance/staff/approve/[id]/route.jsx
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { , NextResponse } from "next/server";
 import connect from "@/database/db";
 import { ClearanceSubmission, Staff } from "@/lib/models";
 
